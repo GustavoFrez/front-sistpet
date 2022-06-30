05ac29f4a8e95bb556f11e0d1eb794a7669ee7f7
Gustavo Frez <gustavo.frez@conexasaude.com.br>
2022-06-30 00:35:48 -0300

    Mudanças das rotas

--- frontEnd/js/index.js
+++ frontEnd/js/index.js
@@ -1,23 +1,26 @@
-$('#btnLogin').click(()=>{
-    let login = {
-        login: $('#inputLogin').val(),
-        senha: $('#inputSenha').val()
-    }
-    axios.post('http://localhost:2424/adotante/login',login).then(response => {
-        let dados = response.data
-        console.log(dados)
-        if(dados.Ok){
-            sessionStorage.userLogin = dados.user.login
-            sessionStorage.userNome = dados.user.nome
-            sessionStorage.userCpf = dados.user.cpf
-            sessionStorage.userSexo = dados.user.sexo
-            sessionStorage.userEmail = dados.user.email
-            sessionStorage.userNascimento = dados.user.dataDeNascimento
-            sessionStorage.userNascimento = dados.user.tel
-            sessionStorage.userNascimento = dados.user.endereco
+$("#btnLogin").click(() => {
+  let login = {
+    login: $("#inputLogin").val(),
+    senha: $("#inputSenha").val(),
+  };
+  axios
+    .post("https://sistspet.herokuapp.com/adotante/login", login)
+    .then((response) => {
+      let dados = response.data;
+      console.log(dados);
+      if (dados.Ok) {
+        sessionStorage.userLogin = dados.user.login;
+        sessionStorage.userNome = dados.user.nome;
+        sessionStorage.userCpf = dados.user.cpf;
+        sessionStorage.userSexo = dados.user.sexo;
+        sessionStorage.userEmail = dados.user.email;
+        sessionStorage.userNascimento = dados.user.dataDeNascimento;
+        sessionStorage.userNascimento = dados.user.tel;
+        sessionStorage.userNascimento = dados.user.endereco;
 
-            window.location.href = ('/frontEnd/pets.html') 
-        }
-        else{ alert(dados.erro)}
-    })
-}) 
+        window.location.href = "/frontEnd/pets.html";
+      } else {
+        alert(dados.erro);
+      }
+    });
+});
